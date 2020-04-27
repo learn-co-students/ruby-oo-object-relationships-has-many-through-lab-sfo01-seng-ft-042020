7e8b25bb2fe6a940e05d5976ee2deef8f9dc1b5e
James <jmcelroy@plexustech.net>
2020-04-27 16:33:04 -0500

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,14 @@
+require 'pry'
+require_relative './appointment'
+require_relative './patient'
+require_relative './doctor'
+
+
+# seeds, dummy data (2 patients, 2 doctors, 2 appointments)
+
+
+
+
+
+binding.pry
+0
